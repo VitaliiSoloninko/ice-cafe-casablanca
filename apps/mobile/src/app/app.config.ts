1114d4fb12dafae4e1c47c5b674e98e460ed68ef
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-01-18 19:18:55 +0100

    feat(mobile config): add capacitor.config, styles app config

--- apps/mobile/src/app/app.config.ts
+++ apps/mobile/src/app/app.config.ts
@@ -1,10 +1,20 @@
 import {
   ApplicationConfig,
+  importProvidersFrom,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    importProvidersFrom(
+      IonicModule.forRoot({
+        mode: 'md', // Material Design mode for Android
+      }),
+    ),
+  ],
 };
--- apps/mobile/src/app/app.ts
+++ apps/mobile/src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [IonApp, IonRouterOutlet, RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'mobile';
+  protected title = 'Ice Cafe Casablanca Mobile';
 }
