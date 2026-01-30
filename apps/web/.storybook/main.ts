1fa57ab2e7548bc7d57f6649f942397e18ea2b7d
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-01-30 22:26:22 +0100

    feat(storybook): add to storybook web: shared library

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,12 +1,17 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../src/app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  stories: [
+    '../src/app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+    '../../../shared/**/src/lib/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+    '../../../features/src/lib/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+  ],
   addons: [],
   framework: {
     name: '@storybook/angular',
     options: {},
   },
+  staticDirs: [{ from: '../../../shared/assets/src/lib', to: '/assets' }],
 };
 
 export default config;
